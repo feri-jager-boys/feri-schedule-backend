ad1fed62d38467e98fb1bd2d08458c8d1dacc69a
Miha Mok <miha.mok@impol.si>
2024-11-03 21:15:06 +0100

    scraping each week

--- implementations/scraper.js
+++ implementations/scraper.js
@@ -1,8 +1,10 @@
 const puppeteer = require("puppeteer");
 
-const url = "https://www.wise-tt.com/wtt_um_feri/index.jsp?filterId=0;389,569;0;0;"; // RIT MAG 1.letnik
+const url =
+    "https://www.wise-tt.com/wtt_um_feri/index.jsp?filterId=0;389,569;0;0;"; // RIT MAG 1.letnik
 
-const openWeekXPath = '//*[@id="form:j_idt149"]/div[3]';
+const openWeekXPath = '//*[@id="form:j_idt147"]';
+const currentWeekXPath = '//*[@id="form:j_idt147_input"]';
 
 const startTime = 7;
 
@@ -15,52 +17,78 @@ const getFullSchedule = async (result) => {
 
         const page = await browser.newPage();
 
-        await page.goto((url), {
+        await page.goto(url, {
             waitUntil: "domcontentloaded",
         });
 
-        const openWeek = await page.$x(openWeekXPath);
+        const [selectedWeekElement] = await page.$x(currentWeekXPath);
 
-        if (openWeek.length > 0) {
-            await openWeek[0].click();
-            const selectWeek = await page.$x(selectWeekXPath);
+        const selectedWeekValue = await selectedWeekElement.evaluate(
+            (select) => select.value
+        );
 
-            if (selectWeek.length > 0) {
-                await selectWeek[0].click();
+        const [selectWeek] = await page.$x(openWeekXPath);
+
+        await selectWeek.click();
+        for (let i = 0; i < selectedWeekValue; i++) {
+            await page.keyboard.press("ArrowUp");
+        }
+        await page.keyboard.press("Enter");
+
+        let schedule = [];
+
+        for (let i = 1; i < 52; i++) {
+            if (i !== 1) {
+                const [selectWeek] = await page.$x(openWeekXPath);
+                await selectWeek.click();
+                await page.keyboard.press("ArrowDown");
+                await page.keyboard.press("Enter");
+            }
+
+            await new Promise((resolve) => setTimeout(resolve, 500));
+
+            const calendarTable = await page.$("#mainCalendar");
+            if (calendarTable) {
+                await getScheduleForWeek(calendarTable, schedule, i);
+            } else {
+                console.error("Calendar table not found after DOM change.");
             }
         }
 
-        setTimeout(async () => {
-            const calendarTable = await page.$('#mainCalendar')
-            const schedule = await getSchedule(calendarTable);
-            resolve(schedule);
-
-            await browser.close();
-        }, 2000);
+        resolve(schedule);
+        await browser.close();
     });
-}
+};
 
-const getSchedule = async (calendarTable) => {
-    let schedule = [];
-
+const getScheduleForWeek = async (calendarTable, schedule, week) => {
     if (calendarTable) {
-        const inputs = await calendarTable.$$('input');
+        const inputs = await calendarTable.$$("input");
 
         for (let i = 0; i < inputs.length; i++) {
-            const value = await inputs[i].evaluate(element => element.getAttribute('value'));
-            const id = await inputs[i].evaluate(element => element.getAttribute('id'));
+            const value = await inputs[i].evaluate((element) =>
+                element.getAttribute("value")
+            );
+            const id = await inputs[i].evaluate((element) =>
+                element.getAttribute("id")
+            );
 
             if (id && id.endsWith("Hour")) {
                 if (value) {
-                    const subjectWithType = await inputs[i + 2].evaluate(element => element.getAttribute('value'))
-                    const groupValue = await inputs[i + 1].evaluate(element => element.getAttribute('value'))
+                    const subjectWithType = await inputs[i + 2].evaluate((element) =>
+                        element.getAttribute("value")
+                    );
+                    const groupValue = await inputs[i + 1].evaluate((element) =>
+                        element.getAttribute("value")
+                    );
 
                     const lastSpaceIndex = subjectWithType.lastIndexOf(" ");
 
-                    const type = subjectWithType.substring(lastSpaceIndex + 1).replace(/[()]/g, '');
+                    const type = subjectWithType
+                        .substring(lastSpaceIndex + 1)
+                        .replace(/[()]/g, "");
                     const subject = subjectWithType.slice(0, lastSpaceIndex);
 
-                    const valueSplit = value.split(", ")
+                    const valueSplit = value.split(", ");
                     const timeValues = id.split(":");
                     const groupSplit = groupValue.split(" ");
 
@@ -70,9 +98,11 @@ const getSchedule = async (calendarTable) => {
                     const professor = valueSplit[0];
                     const classroom = valueSplit[1];
 
-                    const group = groupSplit[groupSplit.length - 2] + groupSplit[groupSplit.length - 1];
+                    const group =
+                        groupSplit[groupSplit.length - 2] +
+                        groupSplit[groupSplit.length - 1];
 
-                    const time = startTime + (timeIndex / 2);
+                    const time = startTime + timeIndex / 2;
 
                     schedule.push({
                         professor,
@@ -81,8 +111,9 @@ const getSchedule = async (calendarTable) => {
                         group,
                         subject,
                         day,
-                        time
-                    })
+                        time,
+                        week,
+                    });
                 }
             }
         }
