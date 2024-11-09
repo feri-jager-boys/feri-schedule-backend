7dd1305c389a10a07a0a5775b42ba64ad88be3aa
amadejpapez <63184600+amadejpapez@users.noreply.github.com>
2024-11-09 20:11:13 +0100

    feat: implement getPrograms endpoint

--- implementations/schedule.js
+++ implementations/schedule.js
@@ -1,4 +1,6 @@
 const Schedule = require('../models/schedule');
+const Program = require('../models/program');
+const Grade = require('../models/grade');
 
 const getFullSchedule = async () => {
     try {
@@ -10,5 +12,46 @@ const getFullSchedule = async () => {
     }
 };
 
+const getPrograms = async () => {
+    try {
+        const programs = await Program.find();
 
-module.exports = {getFullSchedule};
+        const response = [];
+
+        for (const program of programs) {
+            await program;
+
+            const gradeElements = await Grade.find({programId: program._id});
+
+            const gradesResponse = [];
+
+            for (const grade of gradeElements) {
+                const scheduleElements = await Schedule.find({gradeId: grade._id});
+
+                const allGroupNames = scheduleElements.flatMap(schedule => schedule.groups);
+                const uniqueGroupNames = [...new Set(allGroupNames)];
+
+                const uniqueSubjects = [...new Set(scheduleElements.map(schedule => schedule.subject))];
+
+                gradesResponse.push({
+                    grade: grade.grade,
+                    groups: uniqueGroupNames,
+                    subjects: uniqueSubjects,
+                })
+            }
+
+            response.push({
+                id: program._id,
+                name: program.name,
+                gradeItems: gradesResponse
+            })
+        }
+
+        return response;
+    } catch (error) {
+        console.error("Error fetching programs:", error);
+        throw error;
+    }
+};
+
+module.exports = { getFullSchedule, getPrograms };
--- routes/schedule.js
+++ routes/schedule.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const {getFullSchedule} = require('../implementations/schedule');
+const { getFullSchedule, getPrograms } = require('../implementations/schedule');
 
 // schedule/getFull -> get the full schedule from the database
 router.get('/getFull',function(req, res, next) {
     getFullSchedule().then((result) => {
+        res.json({result});
+    });
+});
+
+router.get('/getPrograms',function(req, res, next) {
+    getPrograms().then((result) => {
         res.json({result});
     });
 });
